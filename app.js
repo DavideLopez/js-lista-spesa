7a86f89aea87001f069f833b40aea00201de0dc0
DavideLopez <davidelopez7@gmail.com>
2022-07-27 15:40:52 +0200

    inserito ciclo while

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('spesa')
+
+//WHILE
+
+const listEl = document.getElementById('list')
+
+listEl.innerHTML = ''
+
+const listaSpesa = ['guanciale', 'uova x6', 'pecorino', 'bucatini', 'passata pomodoro',]
+
+let i = 0
+while (i < listaSpesa.length) {
+    const listItem = listaSpesa
+    listEl.innerHTML = `<li>${listItem}</li>`
+    i++
+}
